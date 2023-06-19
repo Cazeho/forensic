ce691038c30a355c68ff1f88b85eba116dd9e9fb
Cazeho <58745332+Cazeho@users.noreply.github.com>
2023-06-19 16:41:33 +0200

    Update forensics.sh

--- forensics.sh
+++ forensics.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+
+# install volatility
 curl -s https://raw.githubusercontent.com/Cazeho/forensic/main/volatility-install.sh | sh
+
+# install peepdf
 curl -s https://raw.githubusercontent.com/Cazeho/forensic/main/peepdf-install.sh | sh
+
+# install extract tools
+
+apt install foremost -y
+apt install pdftk -y
