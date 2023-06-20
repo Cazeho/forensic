c579b74d1e901b854557108c3c36e61df7b9a1a6
Cazeho <58745332+Cazeho@users.noreply.github.com>
2023-06-20 21:09:05 +0200

    Update volatility-install.sh

--- volatility-install.sh
+++ volatility-install.sh
@@ -2,17 +2,18 @@
 
 apt install python2 python2.7-dev libpython2-dev -y
 apt install python-pip -y
-apt install subversion -y
+# apt install subversion -y
 
 pip2 install pycrypto
 pip2 install distorm3
 
 cd /usr/local/lib/python2.7/dist-packages/
 
-svn export https://github.com/volatilityfoundation/volatility/trunk/volatility
+#svn export https://github.com/volatilityfoundation/volatility/trunk/volatility
+#mkdir -p /opt/volatility
+#cd /opt/volatility
 
-mkdir -p /opt/volatility
-cd /opt/volatility
+git clone https://github.com/volatilityfoundation/volatility
 chmod +x vol.py
 
 ln -s $PWD/vol.py /usr/local/bin/volatility
