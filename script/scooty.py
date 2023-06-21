b37063113aa5c436adca09dfe71b24e079fb74c3
Cazeho <58745332+Cazeho@users.noreply.github.com>
2023-06-21 11:34:10 +0200

    Update scooty.py

--- script/scooty.py
+++ script/scooty.py
@@ -12,7 +12,8 @@ scooty --hash (md5|sha) <file_path>
 
 import requests
 import json
-
+import hashlib
+import argparse
 
 VT_API_ENDPOINT = 'https://www.virustotal.com/api/v3/files'
 VT_API_KEY = '7b6e2f3c3a8067581502a841b1a06346772e12c263d49a41e60f948a5b83d6cb'
