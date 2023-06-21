37c5a075b98d398f82987348ec670995f8a548be
Cazeho <58745332+Cazeho@users.noreply.github.com>
2023-06-21 11:22:54 +0200

    Update scooty.py

--- script/scooty.py
+++ script/scooty.py
@@ -3,3 +3,22 @@
 
 
 """
+
+import requests
+import json
+
+
+VT_API_ENDPOINT = 'https://www.virustotal.com/api/v3/files'
+VT_API_KEY = '7b6e2f3c3a8067581502a841b1a06346772e12c263d49a41e60f948a5b83d6cb'
+
+headers = {
+    'x-apikey': VT_API_KEY,
+    "accept": "application/json"
+}
+
+
+files = {'file': ('attachment', attachment_data)}
+# Send the file to VirusTotal for scanning
+response = requests.post(VT_API_ENDPOINT, files=files, headers=headers)
+# Get the scan ID from the response
+scan_id = response.json()['data']['id']
