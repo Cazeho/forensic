fcfa81bae4382cb2e50c6a82af8ec19957fb23c7
Cazeho <58745332+Cazeho@users.noreply.github.com>
2023-06-21 11:47:31 +0200

    Update scooty.py

--- script/scooty.py
+++ script/scooty.py
@@ -15,6 +15,20 @@ import json
 import hashlib
 import argparse
 
+### VT
+
+## send hash
+
+session = requests.Session()
+session.headers = {'X-Apikey': '7b6e2f3c3a8067581502a841b1a06346772e12c263d49a41e60f948a5b83d6cb'}
+
+url = "https://www.virustotal.com/api/v3/search?query=b2a96537b627cc5f7ed63b4b9491b9ea15b08c88dfdd5aeb7a00d903dd4d0176"
+response = session.get(url)
+print(response.text)
+
+
+## send file
+
 VT_API_ENDPOINT = 'https://www.virustotal.com/api/v3/files'
 VT_API_KEY = '7b6e2f3c3a8067581502a841b1a06346772e12c263d49a41e60f948a5b83d6cb'
 
